71961fd1e08e62ddf47fda83aba3714bf6dea968
Swapnanil Dutta <47251193+swapnanildutta@users.noreply.github.com>
2020-06-08 17:43:08 +0530

    Update

--- testing.py
+++ testing.py
@@ -1,4 +1,4 @@
 from pytube import YouTube
-_url='https://www.youtube.com/watch?v=_V_SnyuBCpY&list=PLIivdWyY5sqKiWvnaA5A8F3UQ0Xu5i49U&index=3'
+_url='https://www.youtube.com/watch?v=XLBjvccXX_o'
 yt = YouTube(str(_url))
-print(yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[0].)
+print(yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-1].download())
